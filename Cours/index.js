cec8c343905404b74894380fe6be3715a4bafc8e
Regis JOUAN <regis.jouan@lapiscine.pro>
2023-10-19 14:46:08 +0200

    add numbers and booleans explanations

---
+++ Cours/index.js
@@ -0,0 +1,81 @@
+
+        // Pésentation des opoérateur arithmétiques
+
+/* Commentaire sur plusieurs lignes */
+/** variante de commentaires sur plusieurs lignes */
+
+// var myNumber1 = 9 + 1 
+// var myNumber2 = 4
+
+// myNumber = 6
+
+// var myResult  = myNumber1 * myNumber2
+
+// console.log(myNumber1, myNumber2, myResult)
+
+        // Déclaration moderne (en ES6 , javascript depuis 2015) de variables
+
+// const myNb1 = 9
+// let myNb2 = 3
+
+// myNb2  += 5
+
+// const res = myNb1 * myNb2
+
+// console.log(res)
+
+        // Les chaines de charactères (string)  ""  ''  ``
+
+// let myName = "Je m'appelle Paul"
+// let lastname =  `Doazan`
+// const age = 35;
+// let modifiedAge = age + 5
+
+// let myName2 = ' ' + lastname _ += 7
+// myName2 += ', ' + age + ' ans.'
+
+// const resultStr = `${myName} et j'ai ${modifiedAge} ans.`
+
+// console.log(myName2, myName)
+// console.log(modifiedAge)
+
+
+                // THE BOOLEANS
+
+
+const myBoolean1 = true
+const myBoolean2 = false
+
+let myAge = 18 
+const legalAge = 18
+
+const isAdult = myAge >= legalAge
+
+const is30yearsOld = myAge == 30
+
+// if (isAdult) {
+//     console.log(`Paul est adulte`)
+// }
+// if (!isAdult) {
+//     console.log(`Paul n'est pas adulte`)
+// }
+
+if(isAdult ) {
+    console.log(`Paul est adulte`)
+} else {
+    console.log(`Paul n'est pas adulte`)
+}
+
+if(is30yearsOld ) {
+    console.log(`Paul a trente ans`)
+} else {
+    console.log(`Paul n'a pas trente ans`)
+}
+
+
+
+
+
+
+
+
