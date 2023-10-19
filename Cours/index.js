0b5167e34351c1c4112fa0c5562d4199854e12a7
Regis JOUAN <regis.jouan@lapiscine.pro>
2023-10-19 17:31:07 +0200

    add explanation about following curses

--- Cours/index.js
+++ Cours/index.js
@@ -43,15 +43,15 @@
                 // THE BOOLEANS
 
 
-const myBoolean1 = true
-const myBoolean2 = false
+// const myBoolean1 = true
+// const myBoolean2 = false
 
-let myAge = 18 
-const legalAge = 18
+// let myAge = 18 
+// const legalAge = 18
 
-const isAdult = myAge >= legalAge
+// const isAdult = myAge >= legalAge
 
-const is30yearsOld = myAge == 30
+// const is30yearsOld = myAge == 30
 
 // if (isAdult) {
 //     console.log(`Paul est adulte`)
@@ -73,30 +73,117 @@ const is30yearsOld = myAge == 30
 // }
 
 
-const bigNb1 = 600
-const bigNb2 = 600
+// const bigNb1 = 600
+// const bigNb2 = 600
 
-// Ecrire la condition pour afficher le plus grand nombre
+// // Ecrire la condition pour afficher le plus grand nombre
 
 
-if (bigNb1>bigNb2) {
-    console.log(bigNb1)
-} else if (bigNb1<bigNb2) {console.log(bigNb2)   
-} else if (bigNb1==bigNb2) {
-    console.log(bigNb1, bigNb2)
+// if (bigNb1>bigNb2) {
+//     console.log(bigNb1)
+// } else if (bigNb1<bigNb2) {console.log(bigNb2)   
+// } else if (bigNb1==bigNb2) {
+//     console.log(bigNb1, bigNb2)
+// }
+
+// // const resultComparaison
+
+// const resultComparaison = bigNb1 > bigNb2
+
+// if (resultComparaison) {
+//     console.log(bigNb1)
+// } else {
+//     console.log(bigNb2)
+// }
+
+// const firstname = "Olivier"
+
+
+// if (firstname == "Hamid") {
+//     console.log (`Bonjour mon cher ` + firstname)
+// } else if (firstname == `Pauline`) {
+//     console.log (`Bonjour ma chère ` + firstname)
+// } else {
+// // Réécrire le message loggé avec des backticks ``
+// // consol.log (`Bonjour ` + firstname + ` , je ne connaissais pas ton prénom avant`)
+//     console.log (`Bonjour  ${firstname}  , je ne connaissais pas ton prénom avant.`)
+// }
+
+
+        // variable complexe :les tableaux (Array)
+
+// let arr1 = [3, 5, 12, 204.5, 'hello', false, 'world']
+
+// const firstElement = arr1[4]
+
+// console.log(firstElement)
+// console.log(arr1[4])
+
+// let arrResult1 = arr1[0] + arr1[1]
+
+// console.log(arrResult1)
+
+// let arrResult2 = `${arr1[4]} - ${arr1[6]}`
+
+// console.log(arrResult2)
+
+
+let arr1 = [3, 5, 12, 204.5, 'hello', false, 'world']
+
+let arrResult2
+// On test le booléen du tableau
+if ("truthy") {
+    arrResult2 = `${arr1[4]} - ${arr1[6]}`
+} else { 
+    arrResult2 = arr1[1] + arr1[3]
 }
 
-// const resultComparaison
+// console.log(arrResult2)
 
-const resultComparaison = bigNb1 > bigNb2
+// changement de valeur
+// arr1[4] = "bonjour"
+// console.log(arr1)
 
-if (resultComparaison) {
-    console.log(bigNb1)
-} else {
-    console.log(bigNb2)
+// changer la valeur de 204,5 en 190 par soustraction
+// arr1[3] -= 14.5
+// console.log (arr1)
+
+//             // variable complexe : les objets
+// let obj1 = {
+//     maPremierePropriete: 12,
+//     maDeuxiemePropriete: 14,
+//     prop3: 11,
+//     prop9: 'hello'
+// }
+
+// console.log(obj1.prop9)
+
+
+// A l'aide de l'objet précéedent, construire
+// la chaine de caractère ==> Je m'appelle Paul Doazan, j'ai 35 ans et j'habite à Talence.
+
+let identity1 = {
+    firstname1:'Paul',
+    lastname1: 'Doazen',
+    age1: 35,
+    town1:'Talence'
 }
 
+console.log (`Je m'appelle ${identity1.firstname1} ${identity1.lastname1} , j'ai ${identity1.age1}  et j'habite ${identity1.town1}` )
 
+// Pour ceux qui ont terminé, créer un deuxième objet identity2, 
+// stocker identity et identity 2 dans un tableau, 
+// et écrire la phrse précédente en utilisant le tabaleau.
 
+let identity2 = {
+    firstname2:'Régis',
+    lastname2: 'Jouan',
+    age2: 54,
+    town2:'Bisca'
+}
 
+let arrIdentity1 = [identity1]
+let arrIdentity2 = [firstname2, lastname2, age2, town2]
+
+console.log(arrIdentity1)
 
