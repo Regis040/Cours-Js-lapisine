302466881c4557b748011ac71e8dec3ce9a9fef0
Regis JOUAN <regis.jouan@lapiscine.pro>
2023-10-24 12:53:28 +0200

    maths.around

--- Exercices/imc/index.js
+++ Exercices/imc/index.js
@@ -2,6 +2,7 @@
 const validationBtn = document.querySelector(".validation-btn")
 const resultIMC = document.querySelector('.bmi-value')
 const inputs = document.querySelectorAll('.bmi-input')
+const comment = document.querySelector(`.comment`)
 
 // 2 on déclare un click sur le bouton, à chaque fois qu'on clique, on exécute la fonction associéee
 validationBtn.addEventListener('click', onBtnClick)
@@ -11,10 +12,30 @@ function onBtnClick() {
     const weight = inputs[1].value
     // afficher dans la console l'IMC => poids en kg / taille en m ² ** 2
 
-    // vérifier les données utilisateur, pas de valeurs <= 0
-    const bmi = weight / height ** 2
+   // vérifier les données utilisateur, pas de valeurs <= 0
+   if (!weight || weight < 0) {
+    handleError('heightError')
+    return
+    }
+    if (!height || height < 0) {
+    handleError('weightError')
+    return
+    }
+const bmi = Math.round((weight / height ** 2) * 100) /100
+resultIMC.textContent = bmi
 
-    resultIMC.textContent = bmi
 }
+
+function handleError(typeError) {
+// Implémenter la condition pour préciser le message d'erreur en fonction de la taille ou du poids
+if(typeError== 'heightError') {
+    comment.textContent = "La valeur saisie dans le champ taille est incorrecte"
+}
+if (typeError == 'weightError') {
+    comment.textContent = "La valeur saisie dans le champ poids est incorrecte"    
+}
+}
+ 
+//// autre facon d'y arriver
 
 
